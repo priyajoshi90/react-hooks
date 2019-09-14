e5d068d37706f26e76f6c7260b2cf1904926c540
Priya Joshi <priya.apr9@gmail.com>
2019-09-14 12:03:31 +0530

    custom hook

---
+++ src/hooks/http.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from 'react';
+export const useHttp = (url, dependencies) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [fetchedData, setFetchedData] = useState(null);
+    useEffect(() => {
+        setIsLoading(true);
+        console.log('Sending Http request to', url);
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Failed to fetch.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            setIsLoading(false);
+            setFetchedData(data);
+        })
+        .catch(err => {
+            console.log(err);
+            setIsLoading(false);
+        });
+    }, dependencies);
+    return [isLoading, fetchedData];
+};
